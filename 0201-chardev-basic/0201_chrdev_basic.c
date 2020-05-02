146110296df131d42d81a08fe20fd1a5ee8696d8
Sunil Vaghela <sunilvaghela09@gmail.com>
2020-05-02 13:55:57 +0530

    Updated basic character driver functionality
    Signed-off-by: Sunil Vaghela <sunilvaghela09@gmail.com>

--- 0201-chardev-basic/0201_chrdev_basic.c
+++ 0201-chardev-basic/0201_chrdev_basic.c
@@ -4,50 +4,33 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 
-#define NUM_OF_DEVICES	3
-#define CHRDEV_NAME		"chrdev_basic"
+#define NUM_OF_DEVICES  3
+#define CHRDEV_NAME     "chrdev_basic"
 
 static dev_t dev;
-static struct cdev* p_cdev;
 
 static int __init chrdev_init(void)
 {
-	int ret = 0;
-	ret = alloc_chrdev_region(&dev, 0, NUM_OF_DEVICES, CHRDEV_NAME);
-	if(ret < 0)
-	{
-		printk(KERN_ERR "alloc_chrdev_region failed with error - %d\n", ret);
-		return -1;
-	}
+    int ret = 0;
+    ret = alloc_chrdev_region(&dev, 0, NUM_OF_DEVICES, CHRDEV_NAME);
+    if(ret < 0)
+    {
+        printk(KERN_ERR "alloc_chrdev_region failed with error - %d\n", ret);
+        return -1;
+    }
 
-	printk(KERN_INFO "chrdev_basic devices region allocated\n");
-	printk(KERN_INFO "Major number: %d\nMinor number: %d\n", MAJOR(dev),
-					MINOR(dev));
+    printk(KERN_INFO "chrdev_basic devices region allocated\n");
+    printk(KERN_INFO "Major number: %d\n", MAJOR(dev));
 
-	p_cdev = cdev_alloc();
-	if(p_cdev == NULL)
-	{
-		printk(KERN_ERR "cdev_alloc failed\n");
-		return -1;	
-	}
-
-	ret = cdev_add(p_cdev, dev, NUM_OF_DEVICES);
-	if(ret < 0)
-	{
-		printk(KERN_ERR "cdev_add failed with error - %d\n", ret);
-		return -1;
-	}
-
-	printk(KERN_INFO "chrdev_basic module loaded\n");
-	return 0;
+    printk(KERN_INFO "chrdev_basic module loaded\n");
+    return 0;
 }
 
 
 static void __exit chrdev_exit(void)
 {
-	cdev_del(p_cdev);
-	unregister_chrdev_region(dev, NUM_OF_DEVICES);
-	printk(KERN_INFO "Removed chrdev_basic module\n");
+    unregister_chrdev_region(dev, NUM_OF_DEVICES);
+    printk(KERN_INFO "Removed chrdev_basic module\n");
 }
 
 module_init(chrdev_init);
